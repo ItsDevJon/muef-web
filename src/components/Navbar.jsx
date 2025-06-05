6ed5d8759504631307eae7336b580aa2894f97a2
Jon Ramos <134915416+ItsDevJon@users.noreply.github.com>
2025-06-05 08:44:10 +0200

    Add support for importing SVG files as React components
    - Added `vite-plugin-svgr` to enable importing SVG files as React components
    - Imported `filter-cone.svg` as a React component and replaced it with the material icon to use as the logo in the navigation bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import FilterCone from '../assets/filter-cone.svg?react';
 
 const Navbar = () => {
     return (
         <nav className="container mx-auto mb-12 py-4 px-4 sm:px-6 lg:px-8 flex justify-between items-center">
             <div className="flex items-center space-x-4 gap-2">
                 <div className="flex items-center space-x-2">
-                    <span className="material-icons text-indigo-600">place</span>
+                    {/*<span className="material-icons text-indigo-600">place</span>*/}
+                    <FilterCone />
                     <span className="text-xl font-bold text-gray-900">MUEF</span>
                 </div>
                 <div className="hidden md:flex space-x-6 text-base font-medium text-gray-700">
@@ -24,7 +26,12 @@ const Navbar = () => {
                 </div>
             </div>
             <div className="flex items-center space-x-4">
-                <a href="#" className="text-sm font-medium text-gray-700 hover:text-indigo-600">Sign in</a>
+                <a
+                    href="#"
+                    className="text-sm font-medium text-gray-700 hover:text-indigo-600"
+                >
+                    Sign in
+                </a>
                 <a
                     href="#"
                     className="bg-indigo-600 hover:bg-indigo-700 text-white text-sm font-semibold py-2 px-4 rounded-lg"
